ff303b8e5639f7fff7191ee46e04c4611cca69c6
OLUWATOBI AFINTINNI <131789318+TobyDavids@users.noreply.github.com>
2025-05-19 15:29:33 +0100

    Update Assessment_Q4.sql

--- Assessment_Q4.sql
+++ Assessment_Q4.sql
@@ -5,13 +5,13 @@ WITH transactions AS
 (
 SELECT 
 	owner_id,
-    COUNT(*) AS total_transaction,
-    ROUND(SUM(confirmed_amount/100), 2) AS total_value
+    	COUNT(*) AS total_transaction,
+    	ROUND(SUM(confirmed_amount/100), 2) AS total_value
 FROM
 	savings_savingsaccount
 WHERE 
 	-- Filtering condition to only return valid transations
-    transaction_status IN ('success', 'successful', 'monnify_success')
+    	transaction_status IN ('success', 'successful', 'monnify_success')
 GROUP BY 1
 ),
 -- CTE to derive user tenure
@@ -19,22 +19,23 @@ tenure AS
 (
 SELECT 
 	id AS customer_id,
-    CONCAT(first_name, ' ', last_name) AS name,
+    	CONCAT(first_name, ' ', last_name) AS name,
     -- Using the Timestampdiff function to get the difference between the date a customer joined and the current date in months
-    TIMESTAMPDIFF(MONTH, date_joined, CURDATE()) AS tenure_months
-FROM users_customuser
+    	TIMESTAMPDIFF(MONTH, date_joined, CURDATE()) AS tenure_months
+FROM 
+	users_customuser
 )
 -- Final CLV Calculation for each curstomer
 SELECT 
 	t.customer_id,
-    t.name,
-    t.tenure_months,
-    COALESCE(tr.total_transaction, 0) AS total_transaction,
-    -- CLV formula after ensuring our total transaction and tenure_months isnt zero
-    CASE WHEN tr.total_transaction > 0 AND t.tenure_months > 0
-		 THEN ROUND(((tr.total_transaction/t.tenure_months)* 12 * (tr.total_value*0.001)/tr.total_transaction), 2)
-         ELSE 0
-         END AS estimated_clv
+   	t.name,
+    	t.tenure_months,
+    	COALESCE(tr.total_transaction, 0) AS total_transaction,
+   	 -- CLV formula after ensuring our total transaction and tenure_months isnt zero
+    	CASE WHEN tr.total_transaction > 0 AND t.tenure_months > 0
+	     THEN ROUND(((tr.total_transaction/t.tenure_months)* 12 * (tr.total_value*0.001)/tr.total_transaction), 2)
+             ELSE 0
+             END AS estimated_clv
 FROM 
 	tenure AS t
 LEFT JOIN 
